88deea3f5bf03e8b19cace20cdd531b9aa248422
h4ppyy <93immm@naver.com>
2020-10-22 18:12:31 +0900

    hello

---
+++ components/Sample.vue
@@ -0,0 +1,14 @@
+<template>
+  <div>
+    <h1>hello {{ title }} {{ count }}</h1>
+    <button @click="$emit('plus')">children plus</button>
+    <button @click="$emit('minus')">children minus</button>
+  </div>
+</template>
+
+<script>
+export default {
+  props: ['title', 'count'],
+  emits: ['plus', 'minus']
+}
+</script>
---
+++ pages/index.vue
@@ -0,0 +1,60 @@
+<template>
+  <div>
+    <h1>hello next.js</h1>
+    <div>{{ count }}</div>
+    <NuxtLink to="/about">
+      move about
+    </NuxtLink>
+    <button @click="plus()">parent plus</button>
+    <button @click="minus()">parent minus</button>
+    <Sample 
+      title="title" 
+      :count="count"
+      @plus="plus()"
+      @minus="minus()"
+    />
+    <Sample 
+      title="title" 
+      :count="count"
+      @plus="plus()"
+      @minus="minus()"
+    />
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return { 
+      count: 5 
+    }
+  },
+  methods: {
+    plus() {
+      this.count = this.count + 1;
+    },
+    minus() {
+      this.count = this.count - 1;
+    }
+  },
+  asyncData() {
+    console.log('asyncData')
+    return { content: 'Created at: ' + new Date() }
+  },
+  mounted() {
+    console.log('mounted')
+  },
+  created() {
+    console.log('created')
+  },
+  updated() {
+    console.log('updated')
+  },
+  destroyed() {
+    console.log('destroyed')
+  },
+}
+</script>
+
+<style>
+</style>
